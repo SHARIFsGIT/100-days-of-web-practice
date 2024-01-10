e34ac20f8acfefeb089e611c1b09b912de823219
Shariful <sharifaiub15@gmail.com>
2024-01-10 09:47:57 +0100

    js added

---
+++ Mod 17 (Hello JS)/17.1-17.6.js
@@ -0,0 +1,19 @@
+console.log(5);
+
+var age = 18;
+var price = 580;
+
+var name = 'sharif';
+
+var decision = true;
+
+var user_home_address = "Bremen"; // snake case
+var userHomeAddress = "Bremen"; // camel case
+var UserHomeAddress = "Bremen"; // pascal case
+
+var onionPrice = 20;
+var eggPrice = 20;
+var totalPrice = onionPrice + eggPrice;
+console.log(onionPrice);
+console.log(onionPrice+eggPrice);
+console.log(totalPrice);
