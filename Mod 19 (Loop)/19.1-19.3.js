4c3615b1a60ed973d62305bab1219b4768819f86
Shariful <sharifaiub15@gmail.com>
2024-01-14 19:20:04 +0100

    while loop

---
+++ Mod 19 (Loop)/19.1-19.3.js
@@ -0,0 +1,15 @@
+// if (condition){
+
+// }
+
+var givenNumber = 0;
+while (givenNumber < 7){
+    console.log('Got it ', givenNumber);
+    givenNumber++;
+}
+
+var number = 0;
+while (number <= 10){
+    console.log(number);
+    number += 2;
+}
