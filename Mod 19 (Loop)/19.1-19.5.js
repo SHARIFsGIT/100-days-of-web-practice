b402169925b7516a1dedd00b7f0b6c209349a0c2
Shariful <sharifaiub15@gmail.com>
2024-01-15 14:48:30 +0100

    for loop

--- Mod 19 (Loop)/19.1-19.5.js
+++ Mod 19 (Loop)/19.1-19.5.js
@@ -13,3 +13,12 @@ while (number <= 10){
     console.log(number);
     number += 2;
 }
+
+for (var number = 0; number < 10; number++) {
+    console.log('Got it by for', number);
+}
+
+var numbers = [45, 55, 65, 70, 80, 100, 1];
+for (var i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
