4e7e505553c2c2ec799ee644182894d51ad76b65
Shariful <sharifaiub15@gmail.com>
2024-01-16 18:52:42 +0100

    function added

---
+++ Mod 20 (Functions & Objects)/20.1-20.4.js
@@ -0,0 +1,41 @@
+var numbers = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+var slice = numbers.slice(0, 5)
+console.log(slice);
+
+var name = 'I am learning JavaScript';
+var check1 = name.includes('has');
+var check2 = name.includes('learning');
+console.log(check1, check2);
+console.log(name.substring(0, 6));
+console.log(name[6]); // its not possible name[6] = 'X' thats why array is immutable
+
+// function
+function startFan(){
+    console.log('Stand up');
+    console.log('Go towards');
+    console.log('Switch on');
+}
+startFan();
+
+function bringFruit(money){
+    console.log('Give me');
+    console.log(money);
+}
+bringFruit(100);
+
+function bringSingara(money){
+    console.log('Give me', money, 'taka');
+    console.log('Ai nen', money, 'takar singara');
+}
+var taka = 500;
+bringSingara(taka);
+
+function add(num1, num2){
+    console.log(num1, num2);
+    var sum = num1 + num2;
+    // console.log(sum);
+    // return 15;
+    return sum;
+}
+var total = add(45, 15);
+console.log(total);
