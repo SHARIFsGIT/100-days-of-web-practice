3d6fe91286eb5b3648d74d51e0c8aa3f9382d60e
Shariful <sharifaiub15@gmail.com>
2024-01-19 15:17:00 +0100

    odd in array with odd sum

--- Mod 21 (More JS)/21.6.js
+++ Mod 21 (More JS)/21.6.js
@@ -11,3 +11,17 @@ function getOdd(numbers) {
 const myNumbers = [12, 13, 14, 15, 16, 17, 18, 19, 20];
 const oddArray = getOdd(myNumbers);
 console.log(oddArray);
+
+
+function getSum(numbers){
+    let sum = 0;
+    for(let i=0; i<numbers.length; i++){
+        const index = i;
+        const element = numbers[index];
+        sum += element;
+        console.log('Index:',index, 'Value:', element, 'Sum:', sum);
+    }
+    return sum;
+}
+const oddArraySum = getSum(oddArray);
+console.log(oddArraySum);
