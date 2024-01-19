646c3d2d552d9eea57e3738a2909d39f8ee4b924
Shariful <sharifaiub15@gmail.com>
2024-01-19 18:23:51 +0100

    factorial with while

--- Mod 21 (More JS)/21.7.js
+++ Mod 21 (More JS)/21.7.js
@@ -10,6 +10,7 @@ console.log('Final result:', factorial(5));
 
 
 
+
 function factorialWithRecursive(n) {
     // Base case: factorial of 0 or 1 is 1
     if (n === 0 || n === 1) {
@@ -22,4 +23,33 @@ function factorialWithRecursive(n) {
 const number = 5;
 const result = factorialWithRecursive(number);
 console.log(`The factorial of ${number} is: ${result}`);
-  
+
+
+
+
+function factorialReverse(num) {
+    let result = 1;
+    for (let i = num; i >= 1; i--){
+        result *= i;
+    }
+    return result;
+}
+const factNumber = 5;
+const factorialResult = factorialReverse(factNumber);
+console.log(`Factorial of ${factNumber} is ${factorialResult}`);
+
+
+
+
+function factorialWithWhileLoop(numb) {
+    let i = 1;
+    let result = 1;
+    while (i <= numb){
+        result *= i;
+        i++;
+    }
+    return result;
+}
+const numb = 5;
+const factResult = factorialWithWhileLoop(numb);
+console.log(`Factorial of ${numb} with while loop is ${factResult}`);
