9e467efc8a01476f18c73f4bbb7f13206f108c23
Shariful <sharifaiub15@gmail.com>
2024-01-25 07:20:37 +0100

    duplicate in array

--- Mod 23 (More JS)/23.1-23.3.js
+++ Mod 23 (More JS)/23.1-23.3.js
@@ -19,3 +19,20 @@ console.log('Original:', students);
 
 const partition2 = students.splice(2, 4, 100, 500, 50); // start from 2 and then 4ta element nibe then baki value include korebe array te
 console.log('Original:', students);
+
+
+//duplicate
+const names = ['a', 'b', 'c', 'd', 'e', 'a', 'c', 'd', 'e'];
+
+function removeDuplicate(names){
+    const unique = [];
+    for (let i = 0; i < names.length; i++){
+        const name = names[i];
+        if (unique.includes(name) === false){
+            unique.push(name);
+        }
+    }
+    return unique;
+}
+const uniqueNames = removeDuplicate(names);
+console.log(uniqueNames);
