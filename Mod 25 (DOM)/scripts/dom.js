8a3daf7f4073e8052f95a9457815d8a79ead3b13
Shariful <sharifaiub15@gmail.com>
2024-01-29 03:11:10 +0100

    querySelector

--- Mod 25 (DOM)/scripts/dom.js
+++ Mod 25 (DOM)/scripts/dom.js
@@ -6,3 +6,21 @@ for (const li of liCollection) {
     console.log(li); // gives with tag
     console.log(li.innerText); // gives only text
 }
+const title = document.getElementById('title');
+console.log(title);
+console.log(title.innerText);
+title.innerText = 'Connect'
+
+const importants = document.getElementsByClassName('important');
+for (const important of importants) {
+    console.log(important); // gives with tag
+    console.log(important.innerText); // gives only text
+}
+
+const someLi = document.querySelectorAll('.class li');
+console.log(someLi);
+for (const li of someLi) {
+    console.log(li.innerText); 
+}
+
+// querySelector('.class li'): gives only the first element
