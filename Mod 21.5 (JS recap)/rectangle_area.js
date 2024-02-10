230a8284e098378df5dcafd893d10949aa1e34d3
Shariful <sharifaiub15@gmail.com>
2024-02-10 23:34:59 +0100

    area of rectangle

---
+++ Mod 21.5 (JS recap)/rectangle_area.js
@@ -0,0 +1,11 @@
+function findArea(length, width) {
+    var rectangleArea = length * width;
+    return rectangleArea;
+}
+
+var length = 10;
+var width = 5;
+
+var area = findArea(length, width);
+
+console.log(area);
