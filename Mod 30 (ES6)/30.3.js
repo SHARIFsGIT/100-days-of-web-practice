be6adde712aa4397d182594b6214396b7282cc98
Shariful <sharifaiub15@gmail.com>
2024-02-17 13:54:00 +0100

    Template String, Multiple Line String, Dynamic String

---
+++ Mod 30 (ES6)/30.3.js
@@ -0,0 +1,22 @@
+const first = 'Sharif'
+const last = 'Islam'
+const name = first + ' ' + last;
+console.log(name);
+
+const a = 10;
+const b = 100;
+const result = 'The sum of ' + a + ' and ' + b + ' is ' + (a + b);
+console.log(result);
+
+const c = 100;
+const d = 100;
+const result2 = `The sum of ${c} and ${d} is ${c+d}`;
+console.log(result2);
+
+const email = 'Hi Jhon \n' +
+'Thabor dimo';
+console.log(email);
+
+const challenge = `Tor
+khobor ase`
+console.log(challenge);
