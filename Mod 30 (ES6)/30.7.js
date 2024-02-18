529f4a812cda5f431a1cbc30be1e645ce5459d95
Shariful <sharifaiub15@gmail.com>
2024-02-18 06:19:59 +0100

    Object And Array Destructure

---
+++ Mod 30 (ES6)/30.7.js
@@ -0,0 +1,28 @@
+// distructure
+const actor = {
+    name: 'Ananta',
+    age: 36,
+    phone: '01795394787',
+    money: 800
+}
+console.log(actor.phone)
+
+const age = actor.age;
+console.log(age)
+
+const {money, name, phone: number} = actor; // distructure & alias in phone
+console.log(money)
+console.log(name)
+console.log(number)
+
+
+const numbers = [45, 99]
+const [first, second] = numbers;
+const [third, fourth] = [55, 100]
+
+// distructure in function
+function doubleThem(a, b){
+    return [a*2, b*2];
+}
+const [prothom, ditiyo] = doubleThem(6, 9);
+console.log(prothom, ditiyo)
