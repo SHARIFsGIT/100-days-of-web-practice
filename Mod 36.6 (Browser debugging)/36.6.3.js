4a0d730dea0a01f9c06c8635b9b1a6d1b3cf048a
Shariful <sharifaiub15@gmail.com>
2024-03-18 09:10:25 +0100

    console table/count/monitor

---
+++ Mod 36.6 (Browser debugging)/36.6.3.js
@@ -0,0 +1,5 @@
+location.assign('https://web.programming-hero.com/update-1/video/update-1-36_6-3-location-url-parts-history-api')
+
+console.count('');
+console.table(variable);
+monitor(console.log(object));
