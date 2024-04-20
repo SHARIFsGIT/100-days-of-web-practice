444b6d47585a248cbc3ca6b7ea8c3484bf01bf14
Shariful <sharifaiub15@gmail.com>
2024-04-20 06:03:52 +0200

    react awesome

--- Mod 44 (React router)/44.1/src/components/Phones/Phones.jsx
+++ Mod 44 (React router)/44.1/src/components/Phones/Phones.jsx
@@ -1,12 +1,57 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { DNA } from "react-loader-spinner";
+import { Bar, BarChart, Tooltip, XAxis, YAxis } from "recharts";
 
 const Phones = () => {
-    const [] = useState([])
-    return (
-        <div>
-            
+  const [phones, setPhones] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+    // .then(res => res.json())
+    // .then(data => setPhones(data.data))
+
+    axios
+      .get("https://openapi.programming-hero.com/api/phones?search=iphone")
+      .then((data) => {
+        const phoneData = data.data.data;
+        const phonesWithFakeData = phoneData.map((phone) => {
+          const obj = {
+            name: phone.phone_name,
+            price: parseInt(phone.slug.split("-")[1]),
+          };
+          return obj;
+        });
+        console.log(phonesWithFakeData);
+        setPhones(phonesWithFakeData);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <div>
+      {loading && (
+        <div className="ms-10">
+          <DNA
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="dna-loading"
+            wrapperStyle={{}}
+            wrapperClass="dna-wrapper"
+          />
         </div>
-    );
+      )}
+
+      <h2 className="text-5xl">Phones: {phones.length}</h2>
+      <BarChart width={800} height={400} data={phones}>
+        <Bar dataKey="price" fill="#8884d8" />
+        <XAxis dataKey={"name"}></XAxis>
+        <YAxis></YAxis>
+        <Tooltip></Tooltip>
+      </BarChart>
+    </div>
+  );
 };
 
 export default Phones;
